53ca76e408f4ef556ea65d6688496142594f2114
Atul Varma <varmaa@gmail.com>
2018-12-10 20:53:12 -0500

    Just have 'hello-world' with a '--no-machine' option.

--- nycdbuddy/cli.py
+++ nycdbuddy/cli.py
@@ -3,13 +3,13 @@
 Usage:
   bud.py machine:aws-create [--name=<name>]
   bud.py machine:rm [--name=<name>]
-  bud.py machine:hello-world [--name=<name>]
-  bud.py hello-world
+  bud.py hello-world [--name=<name>] [--no-machine]
   bud.py (-h | --help)
 
 Options:
   -h --help       Show this screen.
-  --name=<name>   docker-machine name to use [default: nycdbuddy].
+  --name=<name>   Docker Machine name to use [default: nycdbuddy].
+  --no-machine    Don't use any Docker Machine.
 '''
 
 from typing import Optional, List
@@ -34,17 +34,15 @@ def hello_world(client: docker.DockerClient) -> None:
 def main(argv: Optional[List[str]]=None) -> None:
     args = docopt.docopt(__doc__, argv=argv)
     name: str = args['--name']
+    no_machine: bool = args['--no-machine']
     if args['machine:aws-create']:
         machine.aws_create(name)
     elif args['machine:rm']:
         machine.rm(name)
-    elif args['machine:hello-world']:
-        client = machine.get_client(name)
-        hello_world(client)
-    elif args['hello-world']:
-        hello_world(docker.client.from_env())
     else:
-        raise AssertionError('we should never reach this point!')
+        client = docker.client.from_env() if no_machine else machine.get_client(name)
+        if args['hello-world']:
+            hello_world(client)
 
 
 if __name__ == '__main__':
